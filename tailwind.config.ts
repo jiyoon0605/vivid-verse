ab158964f853d9bec3c64fa0f27324f1cdfab55d
jiyoon0605 <ljy10499@gmail.com>
2024-05-26 00:03:41 +0900

    project setting - tailwind.config.ts 디폴트 값 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,59 @@
-import type { Config } from 'tailwindcss';
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {},
+        fontSize: {
+            sm: [
+                '12px',
+                {
+                    lineHeight: '18px',
+                    fontWeight: '400',
+                },
+            ],
+            md: [
+                '14px',
+                {
+                    lineHeight: '21px',
+                    fontWeight: '400'
+                }
+            ],
+            button: [
+                '14px',
+                {
+                    lineHeight: '21px',
+                    fontWeight: '700'
+                }
+            ],
+            lg: [
+                '16px',
+                {
+                    lineHeight: '24px',
+                    fontWeight: '400'
+                }
+            ],
+            xl: [
+                '32px',
+                {
+                    lineHeight: '40px',
+                    fontWeight: '700'
+                }
+            ]
+        },
+        colors: {
+            'primary-100': '#71C4EF',
+            'primary-200': '#D4EAF7',
+            'primary-300': '#3B3C3D',
+            'accent-100': '#D4EAF7',
+            'accent-200': '#00668C',
+            'text-100': '#1D1C1C',
+            'text-200': '#313D44',
+            'bg-100': '#FFFEFB',
+            'bg-200': '#F5F4F1',
+            'bg-300': '#CCCBC8',
+        }
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
