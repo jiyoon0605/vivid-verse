0ee445ec7147097aa54a5e7e5e874a95ffb4a0f4
jiyoon0605 <ljy10499@gmail.com>
2024-05-27 22:19:50 +0900

    [ADD] 다국어 처리 - page not found 버그 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en-US', 'ko-KR', 'ja-JP', 'zh_Hans_CN'],
-    defaultLocale: 'en-US',
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
