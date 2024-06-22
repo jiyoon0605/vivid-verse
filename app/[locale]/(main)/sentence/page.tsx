586917970d0f1bb2fab5030cfb5f458f89aa8f19
jiyoon0605 <ljy10499@gmail.com>
2024-06-22 12:27:07 +0900

    [ADD] 문장 페이지 예시 추가

--- app/[locale]/(main)/sentence/page.tsx
+++ app/[locale]/(main)/sentence/page.tsx
@@ -4,12 +4,18 @@ import { DefaultPageProps } from '@/types';
 import SentenceForm from '@/app/[locale]/(main)/sentence/_components/SentenceForm';
 
 export default async function Page({ params: { locale } }: DefaultPageProps) {
-  const t = await getTranslations({ locale, namespace: 'sentence' });
+  const t = await getTranslations({ locale });
 
   return (
     <div>
-      <h1 className={'mt-12 font-bold text-lg animate-appear-right'}>{t('title')}</h1>
-      <p className={'mt-3 lg:w-1/2 animate-[appear-right_0.8s_ease-out]'}>{t('description')}</p>
+      <h1 className={'mt-12 font-bold text-lg animate-appear-right'}>{t('sentence.title')}</h1>
+      <p className={'mt-3 lg:w-1/2 animate-[appear-right_0.8s_ease-out]'}>
+        {t('sentence.description')}
+      </p>
+      <p className={'pt-2 text-text-300'}>
+        ex) {t('example.example_0.pre')}({t('sense.vision')})<br />
+        {'->'} {t('example.example_0.next')}({t('sense.hearing')})
+      </p>
       <SentenceForm />
     </div>
   );
