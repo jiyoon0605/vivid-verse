8a63083d003bd411a56d1bd4d4d36a4738d07683
jiyoon0605 <ljy10499@gmail.com>
2024-07-12 22:18:00 +0900

    [FIX] 프롬포트 수정

--- app/[locale]/text/layout.tsx
+++ app/[locale]/text/layout.tsx
@@ -4,7 +4,7 @@ import NavTab from '@/components/common/NavTab';
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className={'text-text-100'}>
+    <main className={'text-text-100 min-h-dvh max-h-[200dvh]'}>
       <NavTab />
       {children}
     </main>
--- app/[locale]/text/paragraph/result/layout.tsx
+++ app/[locale]/text/paragraph/result/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Layout({ children, result }: { children: ReactNode; result: ReactNode }) {
   return (
-    <div className={'flex max-md:flex-col gap-4 w-full h-full'}>
+    <div className={'flex max-md:flex-col gap-4 w-full my-10'}>
       {children}
       {result}
     </div>
